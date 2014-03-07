f8a06de3b21cd686cf806484e7a65566a2c1adf9
Jeremy Johnson <jeremy@softworks.com.my>
2014-03-07 19:08:06 +0800

    made tests pass
    `test_vice_article_text` was failing so I change it to look for the
    Navigable string.

--- RSS_test.py
+++ RSS_test.py
@@ -1,5 +1,6 @@
 import unittest
 from RSSFeed import RSSobject, ViceRSS
+from bs4.element import NavigableString
 
 class RSSTester(unittest.TestCase):
 
@@ -8,7 +9,6 @@ class RSSTester(unittest.TestCase):
 
     def test_rss_raw_page(self):
         assert '<?xml' in self.RSS.raw
-        print self.RSS.raw
 
     #def rss_soup_page_test(self):
     #    assert 'BBC' in self.RSS.soup.title.string
@@ -24,7 +24,7 @@ class RSSViceTester(unittest.TestCase):
 
     def test_vice_article_text(self):
         self.vice.article_split()
-        self.assertEqual(type(self.vice.articles[0]['title']), str)
+        self.assertEqual(type(self.vice.articles[0]['title']), NavigableString)
 
 if __name__ == "__main__":
     unittest.main()
